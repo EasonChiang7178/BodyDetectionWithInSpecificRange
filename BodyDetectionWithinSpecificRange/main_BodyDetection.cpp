8169ce9cbf6a8ee7e6188f75e6dd58ab9285ee7b
EasonChiange7178 <ishiu.chiang@gmail.com>
2014-11-03 02:41:35 +0900

    With raw config loader

--- BodyDetectionWithinSpecificRange/main_BodyDetection.cpp
+++ BodyDetectionWithinSpecificRange/main_BodyDetection.cpp
@@ -1,19 +1,81 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include "BodyDetection\BodyDetection.h"
 
 using namespace Kinect2;
 using namespace std;
 
+string pathToConfig = "BodyDetection.cfg";
+
 int main(int argc, char** argv) {
 	cv::setUseOptimized(true);
 
+	/* Loading the configuration */
+	fstream fConfig;
+
+	fConfig.open(pathToConfig.c_str(), ios::in);
+	if (fConfig.is_open() == false) {
+		cerr << "> [ERROR]: " << pathToConfig << " doesn't exist!" << endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	string IPAddress;
+	unsigned int Port;
+	double upperRightX, upperRightY,
+		   upperLeftX, upperLeftY,
+		   lowerRightX, lowerRightY,
+		   lowerLeftX, lowerLeftY;
+
+		// Load each line...
+	string lineEntry, entryName, entryValue;
+	while (fConfig.eof() == false) {
+		fConfig >> lineEntry;
+		size_t colonPos = lineEntry.find_last_of(':');
+
+		entryName = lineEntry.substr(0, colonPos);
+		entryValue = lineEntry.substr(colonPos + 1);
+
+		stringstream ss;
+
+		if (entryName == "Address")
+			IPAddress = entryValue;
+		else if (entryName == "Port") {
+			ss << entryValue; ss >> Port;
+		} else if (entryName == "UpperLeftCorner_X") {
+			ss << entryValue; ss >> upperLeftX;
+		} else if (entryName == "UpperLeftCorner_Y") {
+			ss << entryValue; ss >> upperLeftY;
+		} else if (entryName == "UpperRightCorner_X") {
+			ss << entryValue; ss >> upperRightX;
+		} else if (entryName == "UpperRightCorner_Y") {
+			ss << entryValue; ss >> upperRightY;
+		} else if (entryName == "LowerLeftCorner_X") {
+			ss << entryValue; ss >> lowerLeftX;
+		} else if (entryName == "LowerLeftCorner_Y") {
+			ss << entryValue; ss >> lowerLeftY;
+		} else if (entryName == "LowerRightCorner_X") {
+			ss << entryValue; ss >> lowerRightX;
+		} else if (entryName == "LowerRightCorner_Y") {
+			ss << entryValue; ss >> lowerRightY;
+		}
+	}
+
+	fConfig.close();
+
+	/* Starting Body Detection */
 	BodyDetection& bodyDetector = BodyDetection::getDefaultKinectSensor();
+
+	bodyDetector.setRegion(cv::Vec2f(upperLeftX, upperLeftY),
+						   cv::Vec2f(upperRightX, upperRightY),
+						   cv::Vec2f(lowerRightX, lowerRightY),
+						   cv::Vec2f(lowerLeftX, lowerLeftY));
 
 	bodyDetector.initializeColorStream();
 	bodyDetector.initializeBodyStream();
 
-	bodyDetector.connectTo(1234, "192.168.1.132");
+	bodyDetector.connectTo(Port, IPAddress);
 
 	do {
 		bodyDetector.updataStreamData();
